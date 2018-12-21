e380f232236990f781998f62881f81e46f612e8a
Unknown <startkie@gmail.com>
2018-12-22 00:33:56 +1100

    Initial commit

---
+++ Client/desktop/src/com/kizzington/client/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.kizzington.client.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.kizzington.client.Client;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new Client(), config);
+	}
+}
